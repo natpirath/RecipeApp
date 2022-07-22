3e0a176dbbe6a6beecd4ea7af93b0f55cdc2913b
natpirath <natpirath@gmail.com>
2022-07-22 13:26:47 -0400

    Created the RecipePage Activity pushing again hey

---
+++ app/src/main/java/com/cst2335/recipeapp/RecipePage.java
@@ -0,0 +1,14 @@
+package com.cst2335.recipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_page);
+    }
+}
